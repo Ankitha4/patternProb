7b45be808ca7d5e5aace18a7d191093568ae639a
Ankitha Shetty <anki.shetty20@gmail.com>
2025-06-08 10:56:40 +0530

    first commit with Pattern Problems

---
+++ PatternProblem.py
@@ -0,0 +1,76 @@
+n = 5
+
+#square
+for i in range(n):
+    for j in range(n):
+        print("*",end=' ')
+    print()
+
+#increasing Triangle:
+for i in range(n):
+    for j in range(i+1):
+        print("*",end=' ')
+    print()
+
+#decreasing Triangle:
+for i in range(n):
+    for j in range(i,n):
+        print("*",end=' ')
+    print()
+
+#increasing Triangle with space:
+for i in range(n):
+    for j in range(i,n):
+        print(' ',end='')
+    for j in range(i+1):
+        print('*',end='')
+    print()
+    
+#decreasing Triangle with space:
+for i in range(n):
+    for j in range(i+1):
+        print(' ',end='')
+    for j in range(i,n):
+        print('*',end='')
+    print()
+
+#Hill patern
+for i in range(n):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+
+
+#downward hill patern
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+    for j in range(i,n-1):
+        print("*",end="")
+    for j in range(i,n):
+        print("*",end="")
+    print()
+
+
+#Diamond Pattern
+for i in range(n-1):
+    for j in range(i,n):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    for j in range(i+1):
+        print("*",end="")
+    print()
+for i in range(n):
+    for j in range(i+1):
+        print(" ",end="")
+    for j in range(i,n-1):
+        print("*",end="")
+    for j in range(i,n):
+        print("*",end="")
+    print()
+
