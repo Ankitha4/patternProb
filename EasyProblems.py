c0cc8dc854f8e4e2bb42e448f02ea9b8c02098b7
Ankitha Shetty <anki.shetty20@gmail.com>
2025-07-10 22:22:51 +0530

    few solved questions

--- EasyProblems.py
+++ EasyProblems.py
@@ -2,3 +2,4 @@
 tup1=(1,"2",3)
 tup2=(4,2,5)
 print(tup2+tup1)
+
---
+++ InterviewQeutions4.py
@@ -0,0 +1,21 @@
+#reverse a string without uisng functions
+string1 = "Ankithasshetty"
+rev_string=""
+for i in string1:
+    rev_string =  i + rev_string
+print(rev_string)
+
+#find if one string is rotaion of another
+def is_rotaion(s1,s2):
+    s3=s1+s1
+    #print(s1)
+    if s2 in s3:
+        return print(f"{s2} is rotation of {s1}")
+    return print(f"{s2} is not a rotation of {s1}")
+is_rotaion("abc","bac")
+
+#strip all whitespaces from string
+str1=" Anki tha s "
+str1_whitestriped = str1.replace(" ","")
+print(str1_whitestriped)
+
