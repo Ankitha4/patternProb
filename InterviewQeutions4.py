2d995afdf8f75dc2e72abb1c2cc16693bfbd6860
Ankitha Shetty <anki.shetty20@gmail.com>
2025-07-11 07:01:42 +0530

    longest substring without repeatation

--- InterviewQeutions4.py
+++ InterviewQeutions4.py
@@ -19,3 +19,17 @@ str1=" Anki tha s "
 str1_whitestriped = str1.replace(" ","")
 print(str1_whitestriped)
 
+#longest substring without repeating character
+def long_substring_withooutreap(str1):
+    charSet= set()
+    l=0
+    lswr=0
+    for r in range(len(str1)):
+        while str1[r] in charSet:
+            charSet.remove(str1[l])
+            l +=1
+        charSet.add(str1[r])
+        lswr =max(lswr,r-l+1)
+    return print(lswr)
+long_substring_withooutreap("abcdaed")
+
